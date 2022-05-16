7b222ebe6ba8df2671eb5849359bb9eaf22bd44c
Eyad <eyadalsaba1989@gmail.com>
2022-05-16 12:52:57 +0200

    cocktailDB project is set up

---
+++ Cocktail/src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+import { BsFillAlarmFill } from "react-icons/bs";
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>React-Template</h1>
+        <p>bake and Shake</p>
+        <BsFillAlarmFill />
+      </header>
+    </div>
+  );
+}
+
+export default App;
